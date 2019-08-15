2351fb70b457f409af27e7ec8eb9b8000b287303
enami tsugutomo <t.enami@sonyged.com>
2019-08-16 08:55:45 +0900

    Mark unused argument so.

--- src/javascripts/pages/generators/python/control.js
+++ src/javascripts/pages/generators/python/control.js
@@ -469,12 +469,12 @@ export function control(ScratchBlocks) {
     throw new Error(`${op}: Unknown mode: ${mode}`);
   };
 
-  ScratchBlocks.Python['timer'] = (block) => {
+  ScratchBlocks.Python['timer'] = (_block) => {
     use_module('koov');
     return [`koov.timer()`, ScratchBlocks.Python.ORDER_ATOMIC];
   };
 
-  ScratchBlocks.Python['reset_timer'] = (block) => {
+  ScratchBlocks.Python['reset_timer'] = (_block) => {
     use_module('koov');
     ScratchBlocks.Python.adjustCurrentLine_(1);
     return `koov.reset_timer()\n`;
